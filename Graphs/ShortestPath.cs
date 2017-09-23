758d0a517dff9660e5543e0a2697b26b2fb89e85
Vikram Kone <vikone@microsoft.com>
2017-09-22 22:36:08 -0700

    shortest path BFS

--- Graphs/ShortestPath.cs
+++ Graphs/ShortestPath.cs
@@ -36,10 +36,65 @@ namespace CodingQuestions
                 return;
             }
 
-            this.FindShortestPath();
+            // this.FindShortestPathDFS();
+            Dictionary<T, T> previous;
+            bool pathFound = this.FindShortestPathBFS(out previous);
+
+            if (pathFound)
+            {
+                List<T> path = new List<T>();
+
+                T currNode = this.end;
+
+                while (!currNode.Equals(this.start))
+                {
+                    path.Add(currNode);
+                    currNode = previous[currNode];
+                }
+
+                path.Add(this.start);
+                path.Reverse();
+
+                Console.WriteLine("Path is {0}", string.Join("->", path));
+            }
         }
 
-        public void FindShortestPath()
+        public bool FindShortestPathBFS(out Dictionary<T, T> previous)
+        {
+            var queue = new Queue<T>();
+            previous = new Dictionary<T, T>();
+
+            queue.Enqueue(this.start);
+
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+
+                if (this.graph.AdjacencyList.ContainsKey(node))
+                {
+                    var neighbors = this.graph.AdjacencyList[node];
+
+                    foreach (var neighbor in neighbors)
+                    {
+                        if (!previous.ContainsKey(neighbor))
+                        {
+                            previous[neighbor] = node;
+
+                            if (neighbor.Equals(this.end))
+                            {
+                                return true;
+                            }
+
+                            queue.Enqueue(neighbor);
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public void FindShortestPathDFS()
         {
             List<T> path = new List<T>();
             HashSet<T> visited = new HashSet<T>();
