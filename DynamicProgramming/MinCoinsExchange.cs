b855e9769872114fb1bb599141fe03fb17bab89f
Vikram Kone <vikone@microsoft.com>
2017-09-25 21:14:53 -0700

    min coins

---
+++ DynamicProgramming/MinCoinsExchange.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingQuestions
+{
+    public class MinCoinsExchange : ISolution
+    {
+        private List<int> notes;
+        private int amount;
+
+        public MinCoinsExchange(List<int> notes, int amount)
+        {
+            this.notes = notes;
+            this.amount = amount;
+        }
+        
+        public void Run()
+        {
+
+        }
+    }
+}
